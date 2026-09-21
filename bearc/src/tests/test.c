60e40d1600c6a754df5a01e58ec2b8bbf81f28a1
zachmahan <zach.mahan.zm@gmail.com>
2026-09-21 11:22:37 -0400

    fix parser test using wrong file

--- bearc/src/tests/test.c
+++ bearc/src/tests/test.c
@@ -127,7 +127,7 @@ br_test_result_t test_parser(void) {
     ASSERT_EQ_ERR("parser/63", 2);
     ASSERT_EQ_ERR("parser/64", 1);
     ASSERT_EQ_ERR("parser/65", 0);
-    ASSERT_EQ_ERR("parser/65", 2);
+    ASSERT_EQ_ERR("parser/66", 2);
 
     return TEST_RESULT;
 }
